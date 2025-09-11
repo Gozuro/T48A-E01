c402e51830442ad5eeb8a4617f88a02174150d22
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-09-11 08:19:33 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -12,7 +12,7 @@ import numpy as np
 # Importante: verifica que tu nombre y número de matrícula esten correctos
 nombre = ""
 numero_de_matricula = 000000
-fecha = '2025-02-26'
+fecha = '2025-11-09'
 
 """1.   El 30 de junio de 1992, la capitalización de mercados de valores del Pacífico y Asia fue:
 
